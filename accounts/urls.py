28aa93c497a6344d1b1aeb015904d1dcf5aa4bc7
AP3HIM <thefartz19@gmail.com>
2025-06-21 18:41:15 -0400

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from .views import (
     ProtectedView,
     user_profile,
     ResendConfirmationView,
+    redirect_confirm_email,
 )
 from dj_rest_auth.registration.views import VerifyEmailView
 
@@ -13,6 +14,6 @@ urlpatterns = [
     path('protected/', ProtectedView.as_view(), name='protected'),
     path('profile/', user_profile),
     path("resend-confirm/", ResendConfirmationView.as_view()),
-    path('auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),  # ← ADD THIS
+    path('auth/account-confirm-email/<key>/', redirect_confirm_email, name='account_confirm_email'),
     path('auth/', include('dj_rest_auth.registration.urls')),  # <-- this is critical
 ]
