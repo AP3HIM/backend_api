06ef1ef0269e33b54c6076f135364d6b5416a634
AP3HIM <thefartz19@gmail.com>
2025-06-21 17:19:59 -0400

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,6 @@ from .views import (
     ResendConfirmationView,
 )
 from dj_rest_auth.registration.views import VerifyEmailView
-from allauth.account.views import confirm_email
 
 
 urlpatterns = [
@@ -16,5 +15,4 @@ urlpatterns = [
     path("resend-confirm/", ResendConfirmationView.as_view()),
     path('auth/account-confirm-email/', VerifyEmailView.as_view(), name='account_email_verification_sent'),  # ← ADD THIS
     path('auth/', include('dj_rest_auth.registration.urls')),  # <-- this is critical
-    path("confirm-email/<key>/", confirm_email, name="account_confirm_email"),
 ]
