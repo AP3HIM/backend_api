cade89606a2324077444915429ea1d2e0d769119
AP3HIM <thefartz19@gmail.com>
2025-06-26 19:33:56 -0400

    Update urls.py with updated backend

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -4,14 +4,12 @@ from accounts.views import VerifiedEmailTokenView
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('movies.urls')),
-    path('api/', include('accounts.urls')),
+    path('api/movies/', include('movies.urls')),
+    path('api/accounts/', include('accounts.urls')),
     path("api/auth/", include("allauth.urls")),
     path("api/token/", VerifiedEmailTokenView.as_view(), name="token_obtain_pair"),
-    # path("accounts/", include("allauth.urls")),  # Handles confirm-email/<key> with Allauth default view
 ]
 
 if settings.DEBUG:
