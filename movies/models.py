8f0c49b6e517ca74299a95693850c6735286da89
AP3HIM <thefartz19@gmail.com>
2025-07-02 16:49:00 -0400

    Update models.py with slugs for non featured movies

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify  # NEW
+from django.db.models import Q
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=500, unique=True)
@@ -18,8 +20,18 @@ class Movie(models.Model):
     is_hero = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
+        if not self.slug and self.title:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+
+            # Avoid duplicate slugs
+            while Movie.objects.filter(~Q(id=self.id), slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
         super().save(*args, **kwargs)
 
     def __str__(self):
